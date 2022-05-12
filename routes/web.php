6f66559895e5be7c2768fd0398d0fd64d9eb72c0
JuanPedraza <pc.juan.jaeel@gmail.com>
2022-05-12 00:54:42 -0500

    mas cambios

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/login', [RegisterController::class, 'create'])->name('Register.index')
-Route::get('/login', [SessionsController::class, 'create'])->name('Login.index')
+Route::get('/register', [RegisterController::class, 'create'])->name('register.index');
+Route::get('/login', [SessionsController::class, 'create'])->name('login.index');
