cd470e6f8c073c8b1e8af6f202ddb4c1c0c66f9a
JuanPedraza <pc.juan.jaeel@gmail.com>
2022-05-12 01:00:44 -0500

    agregando archivos faltantes

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.App')
+
+@section('tittle', 'login')
+
+@section('content')
+
+    <h1 class="text-5xl text-center pt-24">Login</h1>
+
+
+@endsection 
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.App')
+
+@section('tittle', 'register')
+
+@section('content')
+
+    <h1 class="text-5xl text-center pt-24">Registro</h1>
+
+
+@endsection 
